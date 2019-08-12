3f76392765c257812f910a19812b4447136311d3
ryz2593 <805445415@qq.com>
2019-08-12 14:34:09 +0800

    design pattern

---
+++ happy-study/src/main/java/com/ryz2593/happy/study/design_pattern/factory_pattern/ShapeFactory.java
@@ -0,0 +1,23 @@
+package com.ryz2593.happy.study.design_pattern.factory_pattern;
+
+/**
+ * @author ryz2593
+ * @date 2019/8/12
+ * @desc
+ */
+public class ShapeFactory {
+    //使用 getShape 方法获取形状类型的对象
+    public Shape getShape(String shapeType){
+        if(shapeType == null){
+            return null;
+        }
+        if(shapeType.equalsIgnoreCase("CIRCLE")){
+            return new Circle();
+        } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+            return new Rectangle();
+        } else if(shapeType.equalsIgnoreCase("SQUARE")){
+            return new Square();
+        }
+        return null;
+    }
+}
---
+++ happy-study/src/main/java/com/ryz2593/happy/study/design_pattern/factory_pattern_abstract/asdf.java
@@ -0,0 +1,9 @@
+package com.ryz2593.happy.study.design_pattern.factory_pattern_abstract;
+
+/**
+ * @author ryz2593
+ * @date 2019/8/12
+ * @desc
+ */
+public class asdf {
+}
