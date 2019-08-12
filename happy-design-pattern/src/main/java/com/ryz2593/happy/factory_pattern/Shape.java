357cbc1e96848009a9b5da7511e587d4eff62c5d
ryz2593 <805445415@qq.com>
2019-08-12 14:48:19 +0800

    S

---
+++ happy-design-pattern/src/main/java/com/ryz2593/happy/factory_pattern/Shape.java
@@ -0,0 +1,10 @@
+package com.ryz2593.happy.factory_pattern;
+
+/**
+ * @author ryz2593
+ * @date 2019/8/12
+ * @desc
+ */
+public interface Shape {
+    void draw();
+}
---
+++ happy-design-pattern/src/main/java/com/ryz2593/happy/factory_pattern/Square.java
@@ -0,0 +1,13 @@
+package com.ryz2593.happy.factory_pattern;
+
+/**
+ * @author ryz2593
+ * @date 2019/8/12
+ * @desc
+ */
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
