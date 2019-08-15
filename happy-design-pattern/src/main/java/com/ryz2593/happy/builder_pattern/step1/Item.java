62bf66958be2db809dbc32e1bf33b8a30ce6dc59
ryz2593 <805445415@qq.com>
2019-08-15 13:29:38 +0800

    add

---
+++ happy-design-pattern/src/main/java/com/ryz2593/happy/builder_pattern/step1/Item.java
@@ -0,0 +1,13 @@
+package com.ryz2593.happy.builder_pattern.step1;
+
+/**
+ * 创建一个表示食物条目和食物包装的接口。
+ * @author ryz2593
+ * @date 2019/8/12
+ * @desc
+ */
+public interface Item {
+    public String name();
+    public Packing packing();
+    public float price();
+}
