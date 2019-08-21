b88f236f464a223f50d02d1d7276e777e5253611
ryz2593 <805445415@qq.com>
2019-08-21 13:33:17 +0800

    Binary Tree

---
+++ happy-study/src/main/java/com/ryz2593/happy/study/algorithmbasic/tree/BinaryTreeNode.java
@@ -0,0 +1,46 @@
+package com.ryz2593.happy.study.algorithmbasic.tree;
+
+/**
+ * @author ryz2593
+ * @date 2019/8/21
+ * @desc
+ */
+public class BinaryTreeNode {
+    private int data;
+    private BinaryTreeNode left;
+    private BinaryTreeNode right;
+
+    public BinaryTreeNode() {
+    }
+
+    public BinaryTreeNode(int data, BinaryTreeNode left, BinaryTreeNode right) {
+        super();
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public BinaryTreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BinaryTreeNode left) {
+        this.left = left;
+    }
+
+    public BinaryTreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(BinaryTreeNode right) {
+        this.right = right;
+    }
+}
