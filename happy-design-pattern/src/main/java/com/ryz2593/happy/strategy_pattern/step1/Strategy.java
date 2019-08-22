bef5d760f138297a2dc7b310875798f6f50c5376
ryz2593 <805445415@qq.com>
2019-08-22 14:26:07 +0800

    strategy

---
+++ happy-design-pattern/src/main/java/com/ryz2593/happy/strategy_pattern/step1/Strategy.java
@@ -0,0 +1,10 @@
+package com.ryz2593.happy.strategy_pattern.step1;
+
+/**
+ * @author ryz2593
+ * @date 2019/8/16
+ * @desc
+ */
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
---
+++ happy-design-pattern/src/main/java/com/ryz2593/happy/strategy_pattern/step1/Tour.java
@@ -0,0 +1,10 @@
+package com.ryz2593.happy.strategy_pattern.step1;
+
+/**
+ * @author ryz2593
+ * @date 2019/8/16
+ * @desc
+ */
+public interface Tour {
+    public String traffic(String tool);
+}
