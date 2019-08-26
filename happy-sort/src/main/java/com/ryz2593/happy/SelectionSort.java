e8f8e017c75b7ee9976c0d8e22840e465629ce73
ryz2593 <805445415@qq.com>
2019-08-26 22:33:45 +0800

    selectionSort

--- happy-sort/src/main/java/com/ryz2593/happy/SelectionSort.java
+++ happy-sort/src/main/java/com/ryz2593/happy/SelectionSort.java
@@ -6,4 +6,33 @@ package com.ryz2593.happy;
  * @desc
  */
 public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,30,18,25,40,6,12};
+        int[] selectionSort = selectionSort(arr);
+        for (int i : selectionSort) {
+            System.out.print(i + " ");
+        }
+    }
+
+    /**
+     * 选择排序
+     * @param array
+     * @return
+     */
+    public static int[] selectionSort(int[] array) {
+        if (array.length == 0)
+            return array;
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) //找到最小的数
+                    minIndex = j; //将最小数的索引保存
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
 }
