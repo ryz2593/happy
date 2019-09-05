c1a8375801ed999215fe6e9dffa28645be7e423d
ryz2593 <805445415@qq.com>
2019-09-06 00:04:57 +0800

    Update NumberUtil.java

--- happy-common/src/main/java/com/ryz2593/happy/NumberUtil.java
+++ happy-common/src/main/java/com/ryz2593/happy/NumberUtil.java
@@ -6,4 +6,29 @@ package com.ryz2593.happy;
  * @desc
  */
 public class NumberUtil {
+
+    /**
+     * 用JAVA自带的函数
+     * @param str
+     * @return
+     */
+    public static boolean isNumeric(String str) {
+        for (int i = str.length(); --i >= 0; ) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*方法二：推荐，速度最快
+     * 判断是否为整数
+     * @param str 传入的字符串
+     * @return 是整数返回true,否则返回false
+     */
+
+    public static boolean isInteger(String str) {
+        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+        return pattern.matcher(str).matches();
+    }
 }
