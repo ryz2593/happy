139af8b2c25192ed054f206b6cae229baabb3850
ryz2593 <805445415@qq.com>
2019-09-27 09:50:57 +0800

    add dto

---
+++ happy-common/src/main/java/com/ryz2593/happy/StaticIwindowPathInfo.java
@@ -0,0 +1,18 @@
+package com.ryz2593.happy;
+
+import lombok.Data;
+
+@Data
+public class StaticIwindowPathInfo {
+    private Integer id;
+    private String language;
+    private String platform;
+    private Integer sort;
+    private Boolean isEnable;
+    private String hotstyleName;
+    private String htmlName;
+    private String htmlPath;
+    private String hotstyleNumber;
+    private String categoryCode;
+
+}
