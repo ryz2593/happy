bff1fa62949dc597a996e3001e7008652a9e2fb6
ryz2593 <805445415@qq.com>
2019-09-27 09:54:57 +0800

    add info dto

---
+++ happy-common/src/main/java/com/ryz2593/happy/IwindowInfo.java
@@ -0,0 +1,19 @@
+package com.ryz2593.happy;
+
+import lombok.Data;
+
+@Data
+public class IwindowInfo {
+    private String styleNo;
+    private String styleName;
+    private String stylePath;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "styleNo='" + styleNo + '\'' +
+                ", styleName='" + styleName + '\'' +
+                ", stylePath='" + stylePath + '\'' +
+                '}';
+    }
+}
