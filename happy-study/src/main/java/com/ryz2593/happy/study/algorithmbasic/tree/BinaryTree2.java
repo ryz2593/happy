667f1a69997a7ef258e6f5bbf6b2743b7566e4a4
ryz2593 <805445415@qq.com>
2019-10-27 00:29:27 +0800

    Update BinaryTree2.java

--- happy-study/src/main/java/com/ryz2593/happy/study/algorithmbasic/tree/BinaryTree2.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/algorithmbasic/tree/BinaryTree2.java
@@ -12,12 +12,12 @@ public class BinaryTree2 implements Tree {
     //查找节点
     public Node find(int key) {
         Node current = root;
-        while(current != null){
-            if(current.data > key){//当前值比查找值大，搜索左子树
+        while (current != null) {
+            if (current.data > key) {//当前值比查找值大，搜索左子树
                 current = current.leftChild;
-            }else if(current.data < key){//当前值比查找值小，搜索右子树
+            } else if (current.data < key) {//当前值比查找值小，搜索右子树
                 current = current.rightChild;
-            }else{
+            } else {
                 return current;
             }
         }
@@ -27,23 +27,23 @@ public class BinaryTree2 implements Tree {
     //插入节点
     public boolean insert(int data) {
         Node newNode = new Node(data);
-        if(root == null){//当前树为空树，没有任何节点
+        if (root == null) {//当前树为空树，没有任何节点
             root = newNode;
             return true;
-        }else{
+        } else {
             Node current = root;
             Node parentNode = null;
-            while(current != null){
+            while (current != null) {
                 parentNode = current;
-                if(current.data > data){//当前值比插入值大，搜索左子节点
+                if (current.data > data) {//当前值比插入值大，搜索左子节点
                     current = current.leftChild;
-                    if(current == null){//左子节点为空，直接将新值插入到该节点
+                    if (current == null) {//左子节点为空，直接将新值插入到该节点
                         parentNode.leftChild = newNode;
                         return true;
                     }
-                }else{
+                } else {
                     current = current.rightChild;
-                    if(current == null){//右子节点为空，直接将新值插入到该节点
+                    if (current == null) {//右子节点为空，直接将新值插入到该节点
                         parentNode.rightChild = newNode;
                         return true;
                     }
@@ -54,46 +54,48 @@ public class BinaryTree2 implements Tree {
     }
 
     //中序遍历
-    public void infixOrder(Node current){
-        if(current != null){
+    public void infixOrder(Node current) {
+        if (current != null) {
             infixOrder(current.leftChild);
-            System.out.print(current.data+" ");
+            System.out.print(current.data + " ");
             infixOrder(current.rightChild);
         }
     }
 
     //前序遍历
-    public void preOrder(Node current){
-        if(current != null){
-            System.out.print(current.data+" ");
+    public void preOrder(Node current) {
+        if (current != null) {
+            System.out.print(current.data + " ");
             infixOrder(current.leftChild);
             infixOrder(current.rightChild);
         }
     }
 
     //后序遍历
-    public void postOrder(Node current){
-        if(current != null){
+    public void postOrder(Node current) {
+        if (current != null) {
             infixOrder(current.leftChild);
             infixOrder(current.rightChild);
-            System.out.print(current.data+" ");
+            System.out.print(current.data + " ");
         }
     }
+
     //找到最大值
-    public Node findMax(){
+    public Node findMax() {
         Node current = root;
         Node maxNode = current;
-        while(current != null){
+        while (current != null) {
             maxNode = current;
             current = current.rightChild;
         }
         return maxNode;
     }
+
     //找到最小值
-    public Node findMin(){
+    public Node findMin() {
         Node current = root;
         Node minNode = current;
-        while(current != null){
+        while (current != null) {
             minNode = current;
             current = current.leftChild;
         }
@@ -106,58 +108,58 @@ public class BinaryTree2 implements Tree {
         Node parent = root;
         boolean isLeftChild = false;
         //查找删除值，找不到直接返回false
-        while(current.data != key){
+        while (current.data != key) {
             parent = current;
-            if(current.data > key){
+            if (current.data > key) {
                 isLeftChild = true;
                 current = current.leftChild;
-            }else{
+            } else {
                 isLeftChild = false;
                 current = current.rightChild;
             }
-            if(current == null){
+            if (current == null) {
                 return false;
             }
         }
         //如果当前节点没有子节点
-        if(current.leftChild == null && current.rightChild == null){
-            if(current == root){
+        if (current.leftChild == null && current.rightChild == null) {
+            if (current == root) {
                 root = null;
-            }else if(isLeftChild){
+            } else if (isLeftChild) {
                 parent.leftChild = null;
-            }else{
+            } else {
                 parent.rightChild = null;
             }
             return true;
 
             //当前节点有一个子节点，右子节点
-        }else if(current.leftChild == null && current.rightChild != null){
-            if(current == root){
+        } else if (current.leftChild == null && current.rightChild != null) {
+            if (current == root) {
                 root = current.rightChild;
-            }else if(isLeftChild){
+            } else if (isLeftChild) {
                 parent.leftChild = current.rightChild;
-            }else{
+            } else {
                 parent.rightChild = current.rightChild;
             }
             return true;
             //当前节点有一个子节点，左子节点
-        }else if(current.leftChild != null && current.rightChild == null){
-            if(current == root){
+        } else if (current.leftChild != null && current.rightChild == null) {
+            if (current == root) {
                 root = current.leftChild;
-            }else if(isLeftChild){
+            } else if (isLeftChild) {
                 parent.leftChild = current.leftChild;
-            }else{
+            } else {
                 parent.rightChild = current.leftChild;
             }
             return true;
-        }else{
+        } else {
             //当前节点存在两个子节点
             Node successor = getSuccessor(current);
-            if(current == root){
-                root= successor;
-            }else if(isLeftChild){
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
                 parent.leftChild = successor;
-            }else{
+            } else {
                 parent.rightChild = successor;
             }
             successor.leftChild = current.leftChild;
@@ -166,17 +168,17 @@ public class BinaryTree2 implements Tree {
 
     }
 
-    public Node getSuccessor(Node delNode){
+    public Node getSuccessor(Node delNode) {
         Node successorParent = delNode;
         Node successor = delNode;
         Node current = delNode.rightChild;
-        while(current != null){
+        while (current != null) {
             successorParent = successor;
             successor = current;
             current = current.leftChild;
         }
         //后继节点不是删除节点的右子节点，将后继节点替换删除节点
-        if(successor != delNode.rightChild){
+        if (successor != delNode.rightChild) {
             successorParent.leftChild = successor.rightChild;
             successor.rightChild = delNode.rightChild;
         }
