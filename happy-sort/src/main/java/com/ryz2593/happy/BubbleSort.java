c5e3ee0a1bf349e6238faf69ddf417c7512c1063
ryz2593 <805445415@qq.com>
2019-11-09 21:43:03 +0800

    Update BubbleSort.java

--- happy-sort/src/main/java/com/ryz2593/happy/BubbleSort.java
+++ happy-sort/src/main/java/com/ryz2593/happy/BubbleSort.java
@@ -32,6 +32,7 @@ public class BubbleSort {
             return array;
         //外层：需要length-1次循环比较
         for (int i = 0; i < array.length; i++)
+            
             //内层：每次循环需要两两比较的次数，每次比较后，都会将当前最大的数放到最后位置，所以每次比较次数递减一次
             for (int j = 0; j < array.length - 1 - i; j++)
                 if (array[j + 1] < array[j]) {
