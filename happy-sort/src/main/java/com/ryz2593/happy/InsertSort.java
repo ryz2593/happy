1f8dee08c8671c944148ddf095b5dd7c6afc229d
ryz2593 <805445415@qq.com>
2019-11-13 00:00:33 +0800

    Update InsertSort.java

--- happy-sort/src/main/java/com/ryz2593/happy/InsertSort.java
+++ happy-sort/src/main/java/com/ryz2593/happy/InsertSort.java
@@ -16,6 +16,9 @@ public class InsertSort {
         }
     }
 
+    /**
+     * @param array
+     */
     private static void insertSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i+1; j > 0; j--) {
