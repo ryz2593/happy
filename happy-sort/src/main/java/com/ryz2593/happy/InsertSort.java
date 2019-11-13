6930f13a615b2feee23ec01668c13a7737be6779
ryz2593 <805445415@qq.com>
2019-11-14 00:02:05 +0800

    Update InsertSort.java

--- happy-sort/src/main/java/com/ryz2593/happy/InsertSort.java
+++ happy-sort/src/main/java/com/ryz2593/happy/InsertSort.java
@@ -20,6 +20,7 @@ public class InsertSort {
      * 在要排序的一组数中，假定前n-1个数已经排好序，
      * 现在将第n个数插到前面的有序数列中，使得这n个数也是排好顺序的。
      * 如此反复循环，直到全部排好顺序。
+     * 
      * @param array
      */
     private static void insertSort(int[] array) {
