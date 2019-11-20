1f21a59970067b4d496ca96e121fa48376d7820a
ryz2593 <805445415@qq.com>
2019-11-20 13:40:35 +0800

    update RedisTool.java

--- happy-study/src/main/java/com/ryz2593/happy/study/redis/RedisTool.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/redis/RedisTool.java
@@ -2,17 +2,44 @@ package com.ryz2593.happy.study.redis;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Collections;
+
 /**
+ * Redis分布式锁
  * @author ryz2593
  */
 public class RedisTool {
     private static final String LOCK_SUCCESS = "OK";
     private static final String SET_IF_NOT_EXIST = "NX";
     private static final String SET_WITH_EXPIRE_TIME = "PX";
+    private static final Long RELEASE_SUCCESS = 1L;
 
+    /**
+     * 尝试获取分布式锁
+     * @param jedis Redis客户端
+     * @param key 锁
+     * @param requestId 请求标识
+     * @param expireTime 超期时间
+     * @return 是否获取成功
+     */
     public static boolean tryGetDistributedLock(Jedis jedis, String key, String requestId, int expireTime) {
         String result = jedis.set(key, requestId, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, expireTime);
         return LOCK_SUCCESS.equals(result);
     }
+
+    /**
+     * 释放分布式锁
+     * @param jedis Redis客户端
+     * @param key 锁
+     * @param requestId 请求标识
+     * @return 是否释放成功
+     */
+    public static boolean releaseDistributedLock(Jedis jedis, String key, String requestId) {
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        Object eval = jedis.eval(script, Collections.singletonList(key), Collections.singletonList(requestId));
+        return RELEASE_SUCCESS.equals(eval);
+    }
+
+    
 
 }
