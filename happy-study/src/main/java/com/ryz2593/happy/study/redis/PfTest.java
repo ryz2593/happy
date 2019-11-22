888a7fe34e922ee0628bf5364083530d419017b5
ryz2593 <805445415@qq.com>
2019-11-22 17:21:02 +0800

    HyperLogLog 用于统计 如计算网页每天的用户的访问数量UV 可以解决很多精度要求不是很高的统计问题

--- happy-study/src/main/java/com/ryz2593/happy/study/redis/PfTest.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/redis/PfTest.java
@@ -2,6 +2,11 @@ package com.ryz2593.happy.study.redis;
 
 import redis.clients.jedis.Jedis;
 
+/**
+ * HyperLogLog 数据结构进行估数，用于统计 如计算网页每天的用户的访问数量UV
+ * 可以解决很多精度要求不是很高的统计问题
+ * @author ryz2593
+ */
 public class PfTest {
 
     public static void main(String[] args) {
