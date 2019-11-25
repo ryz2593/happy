40ca23f5a390a7bc37d4b9ff6c74ea36dca716d9
ryz2593 <805445415@qq.com>
2019-11-25 10:16:54 +0800

    Redis实现简单限流策略

---
+++ happy-study/src/main/java/com/ryz2593/happy/study/redis/SimpleRateLimiter.java
@@ -0,0 +1,49 @@
+package com.ryz2593.happy.study.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
+
+import java.io.IOException;
+
+/**
+ * 使用Redis实现简单限流策略
+ * @author ryz2593
+ */
+public class SimpleRateLimiter {
+    private Jedis jedis;
+
+    public SimpleRateLimiter(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    /**
+     * @param userId
+     * @param actionKey
+     * @param period
+     * @param maxCount
+     * @return
+     * @throws IOException
+     */
+    public boolean isActionAllowed(String userId, String actionKey, int period, int maxCount) throws IOException {
+        String key = String.format("hist:%s:%s", userId, actionKey);
+        long nowTs  = System.currentTimeMillis();
+        Pipeline pipe = jedis.pipelined();
+        pipe.multi();
+        pipe.zadd(key, nowTs, "" + nowTs);
+        pipe.zremrangeByScore(key, 0, nowTs - period * 1000);
+        Response<Long> count = pipe.zcard(key);
+        pipe.expire(key, period + 1);
+        pipe.exec();
+        pipe.close();
+        return count.get() <= maxCount;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Jedis jedis = new Jedis("172.17.28.129");
+        SimpleRateLimiter limiter = new SimpleRateLimiter(jedis);
+        for (int i = 0; i < 20; i++) {
+            System.out.println(limiter.isActionAllowed("ryz2593", "click", 60, 5));
+        }
+    }
+}
