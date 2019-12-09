7fc00287747effc9ade9eef55da54fe0b30f9f86
ryz2593 <805445415@qq.com>
2019-12-09 09:09:23 +0800

    JedisPool 使用

--- happy-study/src/main/java/com/ryz2593/happy/study/redis/JedisTest.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/redis/JedisTest.java
@@ -3,6 +3,10 @@ package com.ryz2593.happy.study.redis;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+/**
+ *JedisPool 使用
+ * @author ryz2593
+ */
 public class JedisTest {
     public static void main(String[] args) {
         JedisPool pool = new JedisPool();
