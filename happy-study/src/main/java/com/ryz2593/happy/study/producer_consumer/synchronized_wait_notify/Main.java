e2ee1d946e630713e9f8b2f67f0382a2b227cda0
ryz2593 <805445415@qq.com>
2019-12-11 14:28:51 +0800

    ExecutorService

--- happy-study/src/main/java/com/ryz2593/happy/study/producer_consumer/synchronized_wait_notify/Main.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/producer_consumer/synchronized_wait_notify/Main.java
@@ -1,5 +1,7 @@
 package com.ryz2593.happy.study.producer_consumer.synchronized_wait_notify;
 
+import java.util.concurrent.*;
+
 public class Main {
     public static void main(String[] args) {
         Storage storage = new Storage();
@@ -11,6 +13,24 @@ public class Main {
         Thread c2 = new Thread(new Consumer(storage));
         Thread c3 = new Thread(new Consumer(storage));
 
+//        ExecutorService executorService = new ThreadPoolExecutor(3, 3,
+//                0L, TimeUnit.MILLISECONDS,
+//                new LinkedBlockingQueue<Runnable>());
+//        executorService.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                new Producer(storage).run();
+//            }
+//        });
+//        ExecutorService executorService2 = new ThreadPoolExecutor(3, 3,
+//                0L, TimeUnit.MILLISECONDS,
+//                new LinkedBlockingQueue<Runnable>());
+//        executorService2.execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                new Consumer(storage).run();
+//            }
+//        });
         p1.start();
         p2.start();
         p3.start();
