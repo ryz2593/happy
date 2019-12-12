40ba4532ae1e61ea4ef47db8916cf12c819ee2fb
ryz2593 <805445415@qq.com>
2019-12-12 16:51:06 +0800

    使用阻塞队列实现

---
+++ happy-study/src/main/java/com/ryz2593/happy/study/thread/interview/A1B2C3/T09_00_BlockingQueue.java
@@ -0,0 +1,46 @@
+package com.ryz2593.happy.study.thread.interview.A1B2C3;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author ryz2593
+ */
+public class T09_00_BlockingQueue {
+    static BlockingQueue<String> q1 = new ArrayBlockingQueue<>(1);
+    static BlockingQueue<String> q2 = new ArrayBlockingQueue<>(1);
+
+    public static void main(String[] args) {
+        char[] aI = "1234567".toCharArray();
+        char[] aC = "ABCDEFG".toCharArray();
+
+        new Thread(() -> {
+            for (char c : aI) {
+                System.out.println(c);
+                try {
+                    q1.put("ok");
+                    q2.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "t1").start();
+
+        new Thread(() -> {
+            for (char c : aC) {
+
+                try {
+                    q1.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(c);
+                try {
+                    q2.put("ok");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "t2").start();
+    }
+}
