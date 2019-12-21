e559bd3a8fb0cd63e901a64b09c6870b74e82c33
ryz2593 <805445415@qq.com>
2019-12-21 21:16:34 +0800

    Update GuavaBloomTest.java

--- happy-study/src/main/java/com/ryz2593/happy/study/redis/GuavaBloomTest.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/redis/GuavaBloomTest.java
@@ -1,9 +1,26 @@
 package com.ryz2593.happy.study.redis;
 
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
 /**
  * @autor ryz2593
  * @date 2019/12/21 21:10
  * @desc
  */
 public class GuavaBloomTest {
+    //预计要插入多少数据
+    private static final int size = 10000;
+
+    //期望的误判率，误判率不能为0
+private static final double fpp = 0.01;
+
+
+private static BloomFilter<Integer> bloomFilter = BloomFilter.create(Funnels.integerFunnel(), size, fpp);
+
+    public static void main(String[] args) {
+        
+    }
+
+
 }
