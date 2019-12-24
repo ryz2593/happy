a0f802232caedc67e6e75efd3ab8e44e6e0c833a
ryz2593 <805445415@qq.com>
2019-12-24 17:38:31 +0800

    q

---
+++ happy-leetCode/src/main/java/com/ryz2593/happy/MinimumLengthEncoding820.java
@@ -0,0 +1,33 @@
+package com.ryz2593.happy;
+import java.util.Arrays;
+
+/**
+ * 820. 单词的压缩编码
+ * @since 2019-10-23
+ */
+public class MinimumLengthEncoding820 {
+    public int minimumLengthEncoding(String[] words) {
+        Arrays.sort(words, (a, b)->b.length() - a.length());
+        String res = "";
+        for (int i = 0; i < words.length; i++) {
+            int index = res.indexOf(words[i]);
+            if (index != -1 && res.substring(index + words[i].length(), index + words[i].length() + 1).equals("#")) {
+                continue;
+            } else {
+                res = res + words[i];
+                res = res + "#";
+            }
+        }
+        return res.length();
+    }
+
+    public static void main(String[] args) {
+        MinimumLengthEncoding820 ins = new MinimumLengthEncoding820();
+        String[] param1 = new String[] {"time", "me", "bell"};
+        System.out.println(ins.minimumLengthEncoding(param1));
+        String[] param2 = new String[] {"me", "time"};
+        System.out.println(ins.minimumLengthEncoding(param2));
+        String[] param3 = new String[] {"me", "time","member","bell","bear", "bee"};
+        System.out.println(ins.minimumLengthEncoding(param3));
+    }
+}
