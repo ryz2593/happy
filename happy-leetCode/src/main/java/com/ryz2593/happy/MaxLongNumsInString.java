52d3045324d2b52b8f163be36c6bed4360e52998
ryz2593 <805445415@qq.com>
2019-12-24 22:26:39 +0800

    Update MaxLongNumsInString.java

--- happy-leetCode/src/main/java/com/ryz2593/happy/MaxLongNumsInString.java
+++ happy-leetCode/src/main/java/com/ryz2593/happy/MaxLongNumsInString.java
@@ -110,6 +110,10 @@ public class MaxLongNumsInString {
         System.out.println(flag);
     }
 
+    /**
+     * •连续输入字符串，请按长度为8拆分每个字符串后输出到新的字符串数组；
+     * •长度不是8整数倍的字符串请在后面补数字0，空字符串不处理。
+     */
     public static void StringSplit() {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
