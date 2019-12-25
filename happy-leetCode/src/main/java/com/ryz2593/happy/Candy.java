3054b7c996597401d78496eb195985a101798a73
ryz2593 <805445415@qq.com>
2019-12-25 13:13:59 +0800

    candy update

--- happy-leetCode/src/main/java/com/ryz2593/happy/Candy.java
+++ happy-leetCode/src/main/java/com/ryz2593/happy/Candy.java
@@ -1,13 +1,44 @@
 package com.ryz2593.happy;
 
+import java.util.Arrays;
+
 /**
  * @author ryz2593
  */
 public class Candy {
 
     public static void main(String[] args) {
-        int[] ratings = {1, 3, 2, 1};
+        int[] ratings = {1, 3, 2, 1,4,3,1};
         System.out.println(candy(ratings));
+        System.out.println(candySolutionWith2Array(ratings));
+    }
+
+    /**
+     * 使用两个数组来计算
+     * @param ratings
+     * @return
+     */
+    public static int candySolutionWith2Array(int[] ratings) {
+        int sum = 0;
+        int[] left2right = new int[ratings.length];
+        int[] right2left = new int[ratings.length];
+        Arrays.fill(left2right, 1);
+        Arrays.fill(right2left, 1);
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i]>ratings[i-1]) {
+                left2right[i] = left2right[i-1]+1;
+            }
+        }
+        for (int length = ratings.length-2; length > 0; length--) {
+            if (ratings[length]>ratings[length+1]){
+                right2left[length] = right2left[length+1] + 1;
+            }
+        }
+
+        for (int i = 0; i < ratings.length; i++) {
+            sum += Math.max(left2right[i], right2left[i]);
+        }
+        return sum;
     }
 
     public static int candySolution(int[] arr) {
