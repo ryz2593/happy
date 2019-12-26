686e00d8689a38ed6ca6314dd6cd9bf75ab735c5
ryz2593 <805445415@qq.com>
2019-12-26 10:08:29 +0800

    单词压缩编码

---
+++ happy-leetCode/src/main/java/com/ryz2593/happy/WordsMinimumLengthEncoding.java
@@ -0,0 +1,39 @@
+package com.ryz2593.happy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * 给定一个单词列表，我们将这个列表编码成一个索引字符串 S 与一个索引列表 A。
+ *
+ * 例如，如果这个列表是 ["time", "me", "bell"]，我们就可以将其表示为 S = "time#bell#" 和 indexes = [0, 2, 5]。
+ *
+ * 对于每一个索引，我们可以通过从字符串 S 中索引的位置开始读取字符串，直到 "#" 结束，来恢复我们之前的单词列表。
+ *
+ * @author ryz2593
+ */
+public class WordsMinimumLengthEncoding {
+
+    public static void main(String[] args) {
+        String[] words = {"time","me","bell"};
+        System.out.println(minimumLengthEncoding(words));
+    }
+
+    public static int minimumLengthEncoding(String[] words) {
+        Set<String> good = new HashSet(Arrays.asList(words));
+        for (String word: words) {
+            for (int k = 1; k < word.length(); ++k) {
+                good.remove(word.substring(k));
+            }
+        }
+
+        int ans = 0;
+        for (String word: good) {
+            ans += word.length() + 1;
+        }
+        return ans;
+    }
+
+}
