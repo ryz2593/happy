2fa7975b41089a4123ea0d2d90954cc3e88dd277
ryz2593 <805445415@qq.com>
2019-12-26 09:33:34 +0800

    删除字符串中出现次数最少的字符

---
+++ happy-leetCode/src/main/java/com/ryz2593/happy/DeleteTheLeastFrequentCharacterInString.java
@@ -0,0 +1,26 @@
+package com.ryz2593.happy;
+
+import java.util.Scanner;
+
+/**
+ * 实现删除字符串中出现次数最少的字符，若多个字符出现次数一样，则都删除。
+ * 输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
+ *
+ * 字符串只包含小写英文字母, 不考虑非法输入，输入的字符串长度小于等于20个字节。
+ *
+ * @author ryz2593
+ */
+public class DeleteTheLeastFrequentCharacterInString {
+    public static void main(String[] args) {
+
+    }
+
+    public static void deleteLeastFrequentCharacterInString(){
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            char[] chars = str.toCharArray();
+
+        }
+    }
+}
