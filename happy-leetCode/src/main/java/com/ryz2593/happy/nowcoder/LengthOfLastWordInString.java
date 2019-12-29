ffb618e9d6e555ca2a09a4157be1ac252ece552b
ryz2593 <805445415@qq.com>
2019-12-29 20:55:55 +0800

    Create LengthOfLastWordInString.java

---
+++ happy-leetCode/src/main/java/com/ryz2593/happy/nowcoder/LengthOfLastWordInString.java
@@ -0,0 +1,24 @@
+package com.ryz2593.happy.nowcoder;
+
+import java.util.Scanner;
+
+/**
+ * @autor ryz2593
+ * @date 2019/12/29 20:51
+ * @desc
+ */
+public class LengthOfLastWordInString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int lastWordLength = 0;
+        for(int i = str.length() - 1; i >= 0; i--) {
+            while(str.charAt(i) == ' ') {
+                break;
+            }
+            lastWordLength++;
+        }
+
+        System.out.println(lastWordLength);
+    }
+}
