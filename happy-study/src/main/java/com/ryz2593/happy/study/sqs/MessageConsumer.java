8ec8569460061d8f16e020b744475ded00ab1a5e
ryz2593 <805445415@qq.com>
2020-02-27 18:20:35 +0800

    update

--- happy-study/src/main/java/com/ryz2593/happy/study/sqs/MessageConsumer.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/sqs/MessageConsumer.java
@@ -1,10 +1,7 @@
 package com.ryz2593.happy.study.sqs;
 
-import com.google.common.collect.Lists;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import java.util.List;
 
 /**
  * @author ryz2593
