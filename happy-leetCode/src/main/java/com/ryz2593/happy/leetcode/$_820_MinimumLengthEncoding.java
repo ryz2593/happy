7545c16ee9e5f460885bd3d874dbe483a18ff10d
ryz2593 <805445415@qq.com>
2020-03-17 09:13:02 +0800

    update

--- happy-leetCode/src/main/java/com/ryz2593/happy/leetcode/$_820_MinimumLengthEncoding.java
+++ happy-leetCode/src/main/java/com/ryz2593/happy/leetcode/$_820_MinimumLengthEncoding.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
  * 820. 单词的压缩编码
  * @since 2019-10-23
  */
-public class MinimumLengthEncoding820 {
+public class $_820_MinimumLengthEncoding {
     public int minimumLengthEncoding(String[] words) {
         Arrays.sort(words, (a, b)->b.length() - a.length());
         String res = "";
@@ -22,7 +22,7 @@ public class MinimumLengthEncoding820 {
     }
 
     public static void main(String[] args) {
-        MinimumLengthEncoding820 ins = new MinimumLengthEncoding820();
+        $_820_MinimumLengthEncoding ins = new $_820_MinimumLengthEncoding();
         String[] param1 = new String[] {"time", "me", "bell"};
         System.out.println(ins.minimumLengthEncoding(param1));
         String[] param2 = new String[] {"me", "time"};
