b646714c5ec680cc832f23b0d86426b10bc45042
ryz2593 <805445415@qq.com>
2020-03-18 17:24:30 +0800

    update

---
+++ happy-study/src/main/java/com/ryz2593/happy/study/jdk/_String.java
@@ -0,0 +1,14 @@
+package com.ryz2593.happy.study.jdk;
+
+/**
+ * @author ryz2593
+ * @date 2020/3/18 16:08
+ */
+public class _String {
+    public static void main(String[] args) {
+        String s = null;
+        s = new String("asdfasdf");
+
+        System.out.println(s);
+    }
+}
