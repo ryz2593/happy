c22359077af5955d086b27c2b1032a9a20c55454
ryz2593 <805445415@qq.com>
2020-03-26 17:14:24 +0800

    springbean 生命周期

---
+++ happy-study/src/main/java/com/ryz2593/happy/study/spring/beanlifecycle/MyInstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,50 @@
+package com.ryz2593.happy.study.spring.beanlifecycle;
+
+import java.beans.PropertyDescriptor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
+
+/**
+ * @author ryz2593
+ * @date 2020/3/26 16:34
+ */
+
+public class MyInstantiationAwareBeanPostProcessor extends
+        InstantiationAwareBeanPostProcessorAdapter {
+
+    public MyInstantiationAwareBeanPostProcessor() {
+        super();
+        System.out
+                .println("这是InstantiationAwareBeanPostProcessorAdapter实现类构造器！！");
+    }
+
+    // 接口方法、实例化Bean之前调用
+    @Override
+    public Object postProcessBeforeInstantiation(Class beanClass,
+                                                 String beanName) throws BeansException {
+        System.out
+                .println("InstantiationAwareBeanPostProcessor调用postProcessBeforeInstantiation方法");
+        return null;
+    }
+
+    // 接口方法、实例化Bean之后调用
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+        System.out
+                .println("InstantiationAwareBeanPostProcessor调用postProcessAfterInitialization方法");
+        return bean;
+    }
+
+    // 接口方法、设置某个属性时调用
+    @Override
+    public PropertyValues postProcessPropertyValues(PropertyValues pvs,
+                                                    PropertyDescriptor[] pds, Object bean, String beanName)
+            throws BeansException {
+        System.out
+                .println("InstantiationAwareBeanPostProcessor调用postProcessPropertyValues方法");
+        return pvs;
+    }
+}
