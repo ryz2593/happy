e273bcbbcaee164d5fd5fa8030452fc8e41afa81
ryz2593 <805445415@qq.com>
2020-03-27 15:18:53 +0800

    String 方法使用

--- happy-study/src/main/java/com/ryz2593/happy/study/jdk/_String.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/jdk/_String.java
@@ -6,9 +6,24 @@ package com.ryz2593.happy.study.jdk;
  */
 public class _String {
     public static void main(String[] args) {
-        String s = null;
-        s = new String("asdfasdf");
+        String string = "asdfasdf";
 
-        System.out.println(s);
+        System.out.println(string);
+        String anotherString = "asdfasdf";
+        System.out.println(string.equals(anotherString));
+
+        System.out.println(string.contentEquals(anotherString));
+
+        System.out.println(string.compareTo(anotherString));
+
+        System.out.println(string.regionMatches(0, anotherString, 3, 3));
+
+        System.out.println(string.startsWith("a", 4));
+
+        System.out.println(string.hashCode());
+
+        System.out.println(anotherString.hashCode());
+
+
     }
 }
