288d29408eac0ad976ab863a78f260b067e6f59a
ryz2593 <805445415@qq.com>
2020-04-02 09:05:30 +0800

    c3p0数据源

--- happy-study/src/main/java/com/ryz2593/happy/study/threadlocal/transfer/utils/JdbcUtils.java
+++ happy-study/src/main/java/com/ryz2593/happy/study/threadlocal/transfer/utils/JdbcUtils.java
@@ -15,7 +15,32 @@ public class JdbcUtils {
     /**
      * c3p0数据库连接池对象属性
      */
-    private static final ComboPooledDataSource ds = new ComboPooledDataSource("mysql");
+    private static final ComboPooledDataSource ds;
+    // = new ComboPooledDataSource("mysql");
+
+    static {
+        //使用c3p0-config.xml配置文件中的named-config节点中name属性的值
+        ComboPooledDataSource cpds = new ComboPooledDataSource("db");
+        ds = cpds;
+    }
+
+//    static {
+//        //1.初始化C3P0数据源
+//        ds = new ComboPooledDataSource();
+//        // 设置连接数据库需要的配置信息
+//        try {
+//            ds.setDriverClass("com.mysql.jdbc.Driver");
+//            ds.setJdbcUrl("jdbc:mysql://localhost:3306/test");
+//            ds.setUser("root");
+//            ds.setPassword("root");
+//            //2.设置连接池的参数
+//            ds.setInitialPoolSize(5);
+//            ds.setMaxPoolSize(15);
+//
+//        } catch (Exception e) {
+//            throw new ExceptionInInitializerError(e);
+//        }
+//    }
 
     public static Connection getConnection() throws SQLException {
         return ds.getConnection();
