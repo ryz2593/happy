97a62a7005aedf7a033379026b82b34546045c34
ryz2593 <805445415@qq.com>
2020-06-09 19:20:59 +0800

    Create TreeNode.java

---
+++ happy-leetCode/src/main/java/com/ryz2593/happy/leetcode/TreeNode.java
@@ -0,0 +1,16 @@
+package com.ryz2593.happy.leetcode;
+
+/**
+ * 功能描述
+ *
+ * @since 2020-06-09
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
