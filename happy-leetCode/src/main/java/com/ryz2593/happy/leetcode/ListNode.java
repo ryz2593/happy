76167b782f6c93ecf15a7bac188f95ca94e24778
ryz2593 <805445415@qq.com>
2020-06-10 09:13:31 +0800

    Create ListNode.java

---
+++ happy-leetCode/src/main/java/com/ryz2593/happy/leetcode/ListNode.java
@@ -0,0 +1,14 @@
+package com.ryz2593.happy.leetcode;
+/**
+ * ListNode
+ *
+ * @author r30002879
+ * @since 2020-06-10
+ */
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
